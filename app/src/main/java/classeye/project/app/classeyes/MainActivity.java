34838eba3facab238baef1397a7e17eb3d695753
bryan rakoi <brianrakoi@gmail.com>
2019-01-24 14:33:00 +0300

    I added the topNavigation thingy

--- app/src/main/java/classeye/project/app/classeyes/MainActivity.java
+++ app/src/main/java/classeye/project/app/classeyes/MainActivity.java
@@ -1,5 +1,7 @@
 package classeye.project.app.classeyes;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,16 +13,52 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
+    public TabLayout tabLayout;
+    public ViewPager viewPager;
+    public PageAdapter pageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar=findViewById(R.id.MainActivityToolBar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
-        toolbar.setTitle("Class Eye");
         drawerLayout=findViewById(R.id.myDrawerLayout);
+        tabLayout=findViewById(R.id.myTabLayout);
+        viewPager=findViewById(R.id.myViewPager);
+
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.menu);
+        toolbar.setTitle("Class Eye");
+
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign In"));
+        tabLayout.addTab(tabLayout.newTab().setText("Extra"));
+        tabLayout.addTab(tabLayout.newTab().setText("Records"));
+
+        pageAdapter=new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        viewPager.setAdapter(pageAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
 
     }
 }
